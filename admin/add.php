c24ed1bf76823ab0761c8e0236a4821eff61097d
Rahmouchka <rahmasassi2004@gmail.com>
2024-11-25 01:31:39 +0100

    Version3

--- admin/add.php
+++ admin/add.php
@@ -10,12 +10,17 @@
 <body>
     <?php
     require_once "../connexion.php";
-    $sql = "INSERT INTO produit
-    VALUES (NULL,'cable usb',2,200,'Lorem ipsum dolor sit amet consectetur adipisicing elit.','https://picsum.photos/200/200',0";
+    $sql = "INSERT INTO  produit 
+    VALUES (NULL, 'cable vga',1, 200, 'Lorem ipsum 
+    dolor sit amet consectetur adipisicing elit. Architecto 
+    laboriosam cum possimus quod assumenda
+     nihil cupiditate error, repudiandae ipsum neque.',
+      'https://picsum.photos/images/200/200', 0)";
     //envoie de la requete sql
     //avant de fr l'envoie il faut créer $connexion=new Connexion
     $connexion = new Connexion();
     $pdo = $connexion->getConnexion();
+    //envoie de la requête sql
     $res = $pdo->exec($sql);
     echo var_dump($res);
     ?>
--- classes/CRUP_Produit.php
+++ classes/CRUP_Produit.php
@@ -1,2 +1,45 @@
 <?php
-class CRUD_Produit {}
+require_once "produit.php";
+require_once "../connexion.php";
+class CRUD_Produit
+{
+    private $pdo; // objet PDO
+    function __construct()
+    {
+        $connexion = new connexion();
+        $this->pdo = $connexion->getConnexion();
+    }
+    function findAll()
+    {
+        $sql = "select * from produit";
+        $res = $this->pdo->query($sql);
+        return $res->fetchAll(PDO::FETCH_NUM);
+    }
+    function find($id)
+    {
+        $sql = "select * from produit
+        where id=" . $id;
+        $res = $this->pdo->query($sql);
+        return $res->fetch(PDO::FETCH_NUM);
+    }
+    function delete($id)
+    {
+        $sql = "delete from produit where id=$id";
+        $res = $this->pdo->exec($sql);
+        return  $res;
+    }
+    function add(Produit $produit)
+    {
+        $sql = "INSERT INTO produit
+        VALUES (" . $produit->getId() . "," .
+            $produit->getLibelle() . "," .
+            $produit->getPrix() . "," .
+            $produit->getQte() . "," .
+            $produit->getDescription() . "," .
+            $produit->getImage() . "," .
+            $produit->getPromo()
+            . ")";
+        $res = $this->pdo->exec($sql);
+        return $res;
+    }
+}
